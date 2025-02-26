9bef7b1ddc9b9f7e02241dde0b603f11b0ad0c06
Lavanya-lahari <pullalavanya3@gmail.com>
2025-02-27 03:08:58 +0530

    day-1,2

---
+++ day-2-Basic-code/variables.tf
@@ -0,0 +1,20 @@
+variable "ami_id" {
+    description = "inserting ami value"
+    type = string
+    default = "ami-05b10e08d247fb927" 
+  
+}
+
+variable "type" {
+    type = string
+    default = "t2.micro"
+  
+}
+
+
+variable "key" {
+    type = string
+    default = "terraform-key"
+  
+}
+
