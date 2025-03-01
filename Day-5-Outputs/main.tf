5d14404d2928fb8565a3057e589cd1e6e94d8796
Lavanya-lahari <pullalavanya3@gmail.com>
2025-03-01 21:14:07 +0530

    Day-5

--- Day-5-Outputs/main.tf
+++ Day-5-Outputs/main.tf
@@ -4,21 +4,22 @@ resource "aws_instance" "dev" {
     key_name = "terraform-key"
     availability_zone = "us-east-1a"
     tags = {
-        Name = "developer"
+      Name = "dev"
     }
-} 
-
+   
+  
 }
 
 #creating s3 bucket and dynamo DB for state backend storgae and applying the lock mechanisam for statefile
 
 # This backend configuration instructs Terraform to store its state in an S3 bucket.
- terraform {
+terraform {
 backend "s3" {
-    bucket         = "veeranareshitdevops"  # Name of the S3 bucket where the state will be stored.
+    bucket         = "mypracticebucket-00"  # Name of the S3 bucket where the state will be stored.
     region         =  "us-east-1"
-    key            = "day-5/terraform.tfstate" # Path within the bucket where the state will be read/written.
+    key            = "terraform.tfstate" # Path within the bucket where the state will be read/written.
     dynamodb_table = "terraform-state-lock-dynamo" # DynamoDB table used for state locking, note: first run day-4-s3-dynamodb resource then day-4-state-remote-backend
     encrypt        = true  # Ensures the state is encrypted at rest in S3.
 }
 }
+
