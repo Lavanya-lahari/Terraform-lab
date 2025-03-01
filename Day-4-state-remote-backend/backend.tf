a396663d49c78b12b67c9881e43cd8f33b5b94af
Lavanya-lahari <pullalavanya3@gmail.com>
2025-03-01 16:18:12 +0530

    updates

--- Day-4-state-remote-backend/backend.tf
+++ Day-4-state-remote-backend/backend.tf
@@ -1,12 +1,3 @@
-terraform {
- backend "s3" {
-    bucket         = "mydevopsterraformbuckets"  # Name of the S3 bucket where the state will be stored.
-    region         =  "us-east-1"
-    key            = "terraform.tfstate" # Path within the bucket where the state will be read/written.
-    
-}
-}
-
 
 # This backend configuration instructs Terraform to store its state in an S3 bucket.
  terraform {
